46fc7a2b06a96481d0d67330e2a87cbb05a0f846
darren.ladner <dxladner@gmail.com>
2016-04-08 20:14:21 -0500

    First Commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>@yield('title')</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/yeti/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+        <style>
+           /* html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }*/
+        </style>
+    </head>
+
+    <body>
+
+   @include('shared.navbar')
+
+       
+            @yield('content')
+       
+ <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+    </body>
+    
+    </html>
